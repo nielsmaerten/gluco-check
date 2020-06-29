f4fe6fe3cb6a234477800f3e7fb29269695734fd
Niels Maerten <niels@maerten.be>
2020-06-29 21:19:56 +0200

    Add Jest for testing

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export default class GlucoCheck {
+  static sayHello() {
+    return 'Hello, world!';
+  }
+}
---
+++ test/index.spec.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+import GlucoCheck from '../src';
+
+// TODO
+describe('GlucoCheck Module', () => {
+  it('says hello', () => {
+    const greeting = GlucoCheck.sayHello();
+    expect(greeting).toBe('Hello, world!');
+  });
+});
