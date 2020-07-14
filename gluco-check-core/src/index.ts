56ecbb74735d2c62270d2b3610d3687d7672c090
Niels Maerten <niels@maerten.be>
2020-07-15 00:52:56 +0200

    lint fix

--- gluco-check-core/src/index.ts
+++ gluco-check-core/src/index.ts
@@ -1,9 +1,9 @@
 import {DialogflowConversation} from 'actions-on-google';
 
 export const DefaultIntentHandler = (conversation: DialogflowConversation) => {
-  conversation.close("Gluco Check DefaultIntent handler: not yet implemented.")
+  conversation.close('Gluco Check DefaultIntent handler: not yet implemented.');
 };
 
 export const SignInIntentHandler = (conversation: DialogflowConversation) => {
-  conversation.close("Gluco Check DefaultIntent handler: not yet implemented.")
+  conversation.close('Gluco Check DefaultIntent handler: not yet implemented.');
 };
