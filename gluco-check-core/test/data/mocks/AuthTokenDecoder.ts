72f520229f5c923f1e96781158a75572d8db9b9f
Niels Maerten <niels@maerten.be>
2020-08-24 03:02:55 +0200

    fix mock authtokendecoder

--- gluco-check-core/test/data/mocks/AuthTokenDecoder.ts
+++ gluco-check-core/test/data/mocks/AuthTokenDecoder.ts
@@ -1,12 +1,13 @@
+import {ConversationV3} from '@assistant/conversation';
+
 export default class {
-  decodeGoogleUserToken() {
-    return {
-      locale: 'en-US',
+  decodeGoogleUserToken(conversation: ConversationV3) {
+    Object.assign(conversation.user, {
       params: {
         tokenPayload: {
           email: 'test@example.com',
         },
       },
-    };
+    });
   }
 }
