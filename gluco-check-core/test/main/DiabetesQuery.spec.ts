13b50e9e5504db372742c8436dcabdc8ae9d5b3a
Niels Maerten <niels@maerten.be>
2020-08-25 21:30:45 +0200

    add test for diabetesquery

---
+++ gluco-check-core/test/main/DiabetesQuery.spec.ts
@@ -0,0 +1,17 @@
+import DiabetesQuery from '../../src/types/DiabetesQuery';
+import User from '../../src/types/User';
+import {DiabetesPointer} from '../../src/types/DiabetesPointer';
+
+describe('DiabetesQuery', () => {
+  it("expands the 'Everything' type to all other types", () => {
+    const query = new DiabetesQuery(new User(), 'en-US', [DiabetesPointer.Everything]);
+
+    expect(query.pointers).toContain(DiabetesPointer.BloodSugar);
+    expect(query.pointers).toContain(DiabetesPointer.CannulaAge);
+    expect(query.pointers).toContain(DiabetesPointer.CarbsOnBoard);
+    expect(query.pointers).toContain(DiabetesPointer.InsulinOnBoard);
+    expect(query.pointers).toContain(DiabetesPointer.SensorAge);
+    expect(query.pointers).toContain(DiabetesPointer.PumpBattery);
+    expect(query.pointers).not.toContain(DiabetesPointer.Everything);
+  });
+});
