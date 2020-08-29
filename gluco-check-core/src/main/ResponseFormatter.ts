2562e4745d7de8fd51f295e914a1824e2c4e2c2b
Niels Maerten <niels@maerten.be>
2020-08-29 20:55:30 +0200

    start ResponseFormatter

--- gluco-check-core/src/main/ResponseFormatter.ts
+++ gluco-check-core/src/main/ResponseFormatter.ts
@@ -1,16 +1,20 @@
-/* eslint-disable */ // TODO: remove this
 import dQuery from '../types/DiabetesQuery';
 import dSnapshot from '../types/DiabetesSnapshot';
 import aResponse from '../types/AssistantResponse';
+import {ErrorTypes} from '../types/ErrorTypes';
 
+// TODO: next
 export default abstract class ResponseFormatter {
-  public static formatError(errorType: any, query: dQuery): aResponse {
-    // TODO (after NightscoutClient)
-    return new aResponse('', 0, '');
+  public static formatError(errorType: ErrorTypes, query: dQuery): aResponse {
+    return new aResponse('Something went wrong.', Date.now(), query.locale);
   }
 
   public static formatSnapshot(snapshot: dSnapshot, query: dQuery): aResponse {
-    // TODO (after NightscoutClient)
-    return new aResponse('', 0, '');
+    const {glucoseTrend, timestamp, glucoseValue} = snapshot;
+    return new aResponse(
+      `${glucoseValue()} and ${glucoseTrend} as of ${timestamp}`,
+      Date.now(),
+      query.locale
+    );
   }
 }
