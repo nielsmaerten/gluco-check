77de1418e750dd3b89f3b0677943f095e452607c
Niels Maerten <niels@maerten.be>
2020-09-07 01:15:28 +0200

    linting

--- gluco-check-common/compile-yaml.js
+++ gluco-check-common/compile-yaml.js
@@ -4,24 +4,24 @@ const fs = require("fs");
 const path = require("path");
 const glob = require("glob");
 
-glob.sync("**/*.yaml").forEach(yamlFile => {
+glob.sync("**/*.yaml").forEach((yamlFile) => {
   const dir = path.dirname(yamlFile);
   const outFile = path.basename(yamlFile, ".yaml") + ".json";
   const outPath = path.join(dir, outFile);
-  YAML.load(yamlFile, content => {
+  YAML.load(yamlFile, (content) => {
     trim(content);
-    console.log(yamlFile, '->', outPath);
+    console.log(yamlFile, "->", outPath);
     fs.writeFileSync(outPath, JSON.stringify(content));
   });
 });
 
 // Traverse all (nested) keys in the translation file and trim whitespace
 const trim = (obj) => {
-  Object.keys(obj).forEach(key => {
-    if (typeof obj[key] === 'object') {
-      trim(obj[key])
+  Object.keys(obj).forEach((key) => {
+    if (typeof obj[key] === "object") {
+      trim(obj[key]);
     } else {
       obj[key] = String(obj[key]).trim();
     }
-  })
-}
+  });
+};
