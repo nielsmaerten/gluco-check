e9886b81f484af140d899d2d45ce529b8cafe107
Niels Maerten <niels@maerten.be>
2020-09-21 23:48:09 +0200

    leave build dir alone when testing
    just ignore it instead

--- gluco-check-core/jest.config.js
+++ gluco-check-core/jest.config.js
@@ -21,7 +21,7 @@ module.exports = {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: 'coverage',
+  coverageDirectory: '../coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: [
@@ -111,7 +111,7 @@ module.exports = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: '.',
+  rootDir: 'test',
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
