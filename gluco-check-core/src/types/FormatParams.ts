330c527a701f023cd3939aad69b63e986c39123e
Niels Maerten <niels@maerten.be>
2020-10-29 18:20:30 +0100

    add pointer to formatParams

--- gluco-check-core/src/types/FormatParams.ts
+++ gluco-check-core/src/types/FormatParams.ts
@@ -1,6 +1,8 @@
+import {DiabetesPointer} from './DiabetesPointer';
 import DiabetesSnapshot from './DiabetesSnapshot';
 
 export default class FormatParams {
+  public pointer!: DiabetesPointer;
   public snapshot!: DiabetesSnapshot;
   public locale!: string;
   public sayTimeAgo = true;
