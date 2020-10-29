728e990321dce44de9ba7a77a364601adabb654b
Niels Maerten <niels@maerten.be>
2020-10-29 18:18:37 +0100

    add error info, originalQuery and update() to DiabetesSnapshot

--- gluco-check-core/src/types/DiabetesSnapshot.ts
+++ gluco-check-core/src/types/DiabetesSnapshot.ts
@@ -1,12 +1,33 @@
+import {DiabetesPointer} from './DiabetesPointer';
+import DiabetesQuery from './DiabetesQuery';
+import {ErrorTypes} from './ErrorTypes';
 import {GlucoseTrend} from './GlucoseTrend';
 import {GlucoseUnit} from './GlucoseUnit';
 
+const mg_dl_to_mmol_l = (val: number) => Math.round(val / 1.8) / 10;
+
 /**
  * A DiabetesSnapshot represents the state of someone's diabetes at a certain point in time.
- * It will always include a timestamp. All other values are optional.
  */
 export default class DiabetesSnapshot {
   timestamp!: number;
+  originalQuery!: DiabetesQuery;
+
+  constructor(timestamp: number, originalQuery: DiabetesQuery) {
+    this.timestamp = timestamp;
+    this.originalQuery = originalQuery;
+  }
+
+  update(...parts: Partial<DiabetesSnapshot>[]) {
+    parts.forEach(part => {
+      // If the partial snapshot has errors, add these to our errors array
+      part.errors && this.errors.push(...part.errors);
+
+      // Assign all remaining properties
+      delete part.errors;
+      Object.assign(this, part);
+    });
+  }
 
   // Blood sugar
   glucoseTrend?: GlucoseTrend;
@@ -28,9 +49,9 @@ export default class DiabetesSnapshot {
   cannulaInserted?: number;
   pumpBattery?: number;
 
-  constructor(timestamp: number) {
-    this.timestamp = timestamp;
-  }
+  // Optional error info
+  errors: Array<{
+    type: ErrorTypes;
+    affectedPointer: DiabetesPointer;
+  }> = [];
 }
-
-const mg_dl_to_mmol_l = (val: number) => Math.round(val / 1.8) / 10;
