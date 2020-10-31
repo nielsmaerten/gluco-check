a9b7cf934a630cb7916d2b9544c9358458a20496
Niels Maerten <niels@maerten.be>
2020-11-01 00:05:11 +0100

    read last known actionversion from firebase config

--- gluco-check-core/src/main/core/ConversationDecoder.ts
+++ gluco-check-core/src/main/core/ConversationDecoder.ts
@@ -13,10 +13,30 @@ import User from '../../types/User';
  * It takes in a 'conversation' object from Google Actions, and decodes it into a DiabetesQuery
  */
 export default class ConversationDecoder {
+  private lastKnownActionVersion = 0;
+
   constructor(
     private authTokenDecoder: AuthTokenDecoder,
     private userProfileClient: UserProfileClient
-  ) {}
+  ) {
+    // Parse the Action version number from config
+    // You must set this value using the Firebase CLI before deploying
+    // This value decides whether disclaimers are mentioned or not
+    const version = config().google_actions_sdk.glucocheck_action_version;
+    this.lastKnownActionVersion = parseInt(version);
+
+    // Abort if the Action version has not been set
+    if (!this.lastKnownActionVersion) {
+      const errMsg = 'Initialization failed: glucocheck_action_version must be set';
+      logger.error(errMsg);
+      throw new Error(errMsg);
+    } else
+      logger.debug(
+        'Assuming',
+        `v${this.lastKnownActionVersion}`,
+        'is the latest version of the Gluco Check Action being used.'
+      );
+  }
 
   /**
    * Interprets an Assistant Conversation object and returns a DiabetesQuery
@@ -61,20 +81,14 @@ export default class ConversationDecoder {
    */
   shouldMentionDisclaimer(conv: ConversationV3, user: User): boolean {
     const raw_version_currentAction = conv.headers['gluco-check-version'];
-    const raw_version_lastKnown = config().google_actions_sdk.glucocheck_action_version;
 
     const version_currentAction = parseInt(raw_version_currentAction as string);
-    const version_lastKnown = parseInt(raw_version_lastKnown);
+    const version_lastKnown = this.lastKnownActionVersion;
 
-    if (!version_lastKnown) {
-      const errMsg =
-        'Before using this webhook, ' +
-        '[google_actions_sdk.glucocheck_action_version] must be set' +
-        'using the command: firebase functions:config:set';
-      logger.error(errMsg);
-      throw new Error(errMsg);
-    }
     const usingNewerAction = version_currentAction > version_lastKnown;
+    if (usingNewerAction) {
+      logger.info('Force mentioning disclaimer bc of newer Action calling');
+    }
     return user.mentionDisclaimer || usingNewerAction;
   }
 
