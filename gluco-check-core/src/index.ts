1988b9ad0bbe2df86273df609f72d77293eb6e1b
Niels Maerten <niels@maerten.be>
2020-11-30 20:16:44 +0100

    complete todos

--- gluco-check-core/src/index.ts
+++ gluco-check-core/src/index.ts
@@ -6,16 +6,3 @@ import GlucoCheckCore from './main';
 // expose a fully initialized instance of the core library
 const container = new Container({autoBindInjectable: true});
 export default container.get(GlucoCheckCore);
-
-/**
- * Code Cleanup / Refactoring
- * // TODO: Refactoring tasks
- * - Sort imports
- * - Split QueryResolver into QueryFulfiller and ResponseFormatter
- * - Turn DmMetric from an enum into a class???
- *   - Each class could have a humanizer function, extract function, ... ???
- *
- * - Rename:
- *   - DiabetesSnapshot --> DmSnapshot
- *   - DiabetesQuery --> DmQuery
- */
