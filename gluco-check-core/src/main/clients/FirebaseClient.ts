a8d9e83e0892890ee8caf6903594afc54d884ec9
Niels Maerten <niels@maerten.be>
2020-11-30 16:56:14 +0100

    Exclude firebase code from coverage reporting

--- gluco-check-core/src/main/clients/FirebaseClient.ts
+++ gluco-check-core/src/main/clients/FirebaseClient.ts
@@ -1,3 +1,4 @@
+/* istanbul ignore file: External system (Firebase) */
 import {initializeApp, firestore} from 'firebase-admin';
 import {injectable} from 'inversify';
 import {logger} from 'firebase-functions';
