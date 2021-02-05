6064d434d91942e919bddb1aabe1b56ebc516e47
Niels Maerten <niels@maerten.be>
2021-02-06 00:31:37 +0100

    Auto-populate test account with data (#73)
    * Auto-populate reviewer accounts with test data
    
    Reviewer accounts used by Google will automatically receive test configurations upon registration
    
    * Update action settings.yaml
    
    * Add env to Actions
    
    * set prod action version to 2
    
    * codefactor

--- gluco-check-webhooks/src/index.ts
+++ gluco-check-webhooks/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions';
 import conversationHandler from './conversation';
 import validationHandler from './url-validation';
+import newUserHandler from './new-user';
 const logTag = '[Webhook.Main]';
 
 export const validateNightscoutUrl = functions.https.onRequest((req, res) => {
@@ -16,4 +17,8 @@ export const conversation = functions.https.onRequest((request, response) => {
 
   // Pass request and response objects to the Assistant App.
   conversationHandler.Instance(request, response);
+});
+
+export const populateTestAccounts = functions.auth.user().onCreate(user => {
+  return newUserHandler(user);
 });
