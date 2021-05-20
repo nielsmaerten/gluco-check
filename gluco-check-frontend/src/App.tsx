7fcd4c12f6865da791b04667205f12fc56c28fee
Niels Maerten <niels@maerten.be>
2021-05-20 21:12:14 +0200

    Toggle languageSelector using APP-DEBUG
    ... I really shouldn't be committing directly to main, but creating a PR
    for every little hotfix is such a drag 🙃

--- gluco-check-frontend/src/App.tsx
+++ gluco-check-frontend/src/App.tsx
@@ -73,6 +73,10 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+// TODO: Hide the languageSelector on PROD, but leave it enabled during tests
+// Once new languages are available in PROD, this line should be removed
+const showLanguageSelector = APP_DEBUG || process.env.NODE_ENV === "test";
+
 export default function App() {
   const classes = useStyles();
   const [user, loading] = useAuthState(auth);
@@ -98,11 +102,14 @@ export default function App() {
         </section>
         <section className={classes.rightToolbar}>
           <ul className={classes.nav}>
-            {APP_DEBUG && Object.values(AvailableLanguage).length > 1 && (
-              <li>
-                <LanguageSelector />
-              </li>
-            )}
+            {
+              /* TODO: remove this line */ showLanguageSelector &&
+                Object.values(AvailableLanguage).length > 1 && (
+                  <li>
+                    <LanguageSelector />
+                  </li>
+                )
+            }
             <li>
               <IconButton
                 aria-label={t("navigation.faqs")}
