19b463c7f24ccb57df6c3bcbc4118dd921736c30
Niels Maerten <niels@maerten.be>
2022-09-06 20:55:56 +0000

    update title styling

--- gluco-check-frontend/src/pages/Landing.tsx
+++ gluco-check-frontend/src/pages/Landing.tsx
@@ -86,8 +86,12 @@ function Landing() {
         <Grid item className={classes.subtitle}>
           <Container maxWidth="sm">
             <Typography variant="h5" component="h2">
-              <a href="https://pages.glucocheck.app/sunset">Gluco Check is shutting down on June 13th, 2023</a>
-            
+              <a
+                style={{ color: "orange" }}
+                href="https://pages.glucocheck.app/sunset"
+              >
+                Gluco Check is shutting down :( <br /> June 13th, 2023
+              </a>
             </Typography>
           </Container>
         </Grid>
